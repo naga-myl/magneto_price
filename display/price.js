2155e07fa0ba259f0bfea7d5baccb786fe4e5254
naga-myl <nagamylavarapu@gmail.com>
2023-05-14 01:04:11 +0200

    Add to cart is added

--- display/price.js
+++ display/price.js
@@ -1,46 +1,65 @@
 // Includes
 const { Builder, By, Key, until } = require('selenium-webdriver');
+
+const assert = require('assert');
 const should = require('chai').should();
+const expect = require('chai').expect;
+
 
 /*
    I want to search for a product
    and view list of products in
-    sorted by price.
-*/
-              describe('Sort by Price', () => {
+    sorted by price.*/
+
+
+             describe.only('Sort by Price', () => {
                 // Test case:
-                    context('Searched category is displayed', () => {
-                        it('I see the selected product in sorted order', async () => {
+                    context('Search for shirts in search bar and perfom sorting', () => {
+                        it('I see the shirts in sorted order', async () => {
                             // Start web browser
                             const driver = await new Builder().forBrowser('firefox').build();
                             try{
 
-                                await driver.get('https://magento.softwaretestingboard.com/');
-                                 // Get the search input
-                await driver.wait(until.elementLocated(By.css('#search')), 1000);
-                await driver.findElement(By.id('search')).sendKeys('shirt', Key.RETURN);
-                
-               // await driver.sleep(100);
-               
-                    //get the sort         
-                await driver.wait(until.elementLocated(By.css('.toolbar-sorter')),10000);
-               let data=await driver.findElement(By.css('.toolbar-sorter'));
+                                        await driver.get('https://magento.softwaretestingboard.com/');
+                                        // Get the search input
+                                        await driver.wait(until.elementLocated(By.css('#search')), 1000);
+                                        await driver.findElement(By.id('search')).sendKeys('shirt', Key.RETURN);
 
-              let value =await data.findElement(By.css('.sorter-label'));
-              let display=await value.getText();
-              console.log(display);
+                                        
+                                        // await driver.sleep(100);
+                                        //get the sort element and displaying Sort By       
+                                        await driver.wait(until.elementLocated(By.css('.toolbar-sorter')),10000);
+                                        let data=await driver.findElement(By.css('.toolbar-sorter'));
+                                        let value =await data.findElement(By.css('.sorter-label'));
+                                        let display=await value.getText();
+                                        console.log(display);
 
-              //price selecting
-            await driver.wait(until.elementLocated(By.css('.sorter-options')),10000);
-            let price=await driver.findElement(By.css('.sorter-options'));
-           // console.log(price);
+                                        //document.querySelector('.products >li:nth-child(2)');---selecting the 2 child
+                                        //document.querySelector('.product > strong:first-child')--gettinng the product text
+                                        await driver.wait(until.elementLocated(By.css('.products')),10000);
+                                        await driver.findElement(By.css('.products'));
+                                        let valuesort =await driver.findElement(By.css('.products >li:nth-child(2)'));
+                                        //console.log(valuesort);
 
+                                        await driver.wait(until.elementLocated(By.css('.product')),10000);
+                                        await driver.findElement(By.css('.product'));
+                                        let sor= await valuesort.findElement(By.css('.product > strong:first-child')).getText();
+                                        //let displaysort=await value.getText();
+                                        console.log(sor);
 
-            await driver.sleep(1000);
-            let pricevalue=await driver.findElement(By.css('.sorter-options > option:nth-child(2)')).click();
-            //let pricevalueText= await pricevalue.getText();
-          // console.log(pricevalueText);
-              await driver.quit();
+                                        await driver.sleep(2000);
+                                        //price selecting
+                                        await driver.wait(until.elementLocated(By.css('.sorter-options')),10000);
+                                        let price=await driver.findElement(By.css('.sorter-options'));
+                                        await driver.sleep(1000);
+                                        await driver.findElement(By.css('.sorter-options > option:nth-child(2)')).click();
+
+                                       /* assert.equal(sor,'Circe Hooded Ice Fleecee'); // Builtin Node
+                                        expect(sor).to.equal('Circe Hooded Ice Fleecee'); // Chai expect
+                                        sor.should.equal('Circe Hooded Ice Fleece'); // Chai should*/
+
+                                       
+                                       //await driver.quit();
 
                             }
 
---
+++ display/selector.js
@@ -0,0 +1,47 @@
+//addto cart
+
+                             
+
+
+
+                                //document.querySelector('.products >li:first-child').innerText
+                                // document.querySelector('.swatch-attribute >div:nth-child(1)').innerTextt
+                               //displaying the details of the selected Tee
+                            //Selecting Size 
+                                //document.querySelector('#option-label-size-143-item-166 ').innerHTML;
+
+                               // await driver.wait(until.elementLocated(By.css('#option-label-size-143-item-166')),10000);
+                               // await driver.findElement(By.id('option-label-size-143-item-166')).click();
+
+                                //selecting qty
+                                //document.querySelector('.box-tocart> div :nth-child(1)').innerHTML--selecting the qty text
+                                //document.querySelector('#qty').innerText --selecting the text field for qty
+                             /* await driver.wait(until.elementLocated(By.css('.box-tocart')),10000);  
+                               let qtyrea= await driver.findElement(By.css('.box-tocart'));
+                               let qtytextrea= await qty.findElement(By.css('.box-tocart > div :nth-child(1)'));
+                               //accessing the text
+                               await driver.wait(until.elementLocated(By.css('#qty')),10000);
+                               await driver.findElement(By.id('qty')).click();*/
+
+                                
+                                //select different sizes availabe for  Breathe-easy tank 
+                                 /*await driver.wait(until.elementLocated(By.css('.swatch-attribute')),10000);
+                                 let s= await driver.findElement(By.css('.swatch-attribute'));
+                                 let sizes =await cart.findElement(By.css('.swatch-attribute >div:nth-child(1)'));*/
+                                 
+
+                                 //document.querySelector('.product-item-inner').innerHTML;
+                                /* await driver.wait(until.elementLocated(By.css('.product-item-inner')),10000);
+                                 let s1= await driver.findElement(By.css('.product-item-inner'));
+                                 let data=await s1[1].findElement(By.css('.product-item-inner'));
+                                 console.log(data);*/
+                               
+                                 
+
+                                
+                               //Add to cart
+                               //document.querySelector('.product >div:first-child').innerText; (contains all the information for the specific pic selected)
+                               /*await driver.wait(until.elementLocated(By.css('.product')),10000);
+                               let info= await driver.findElement(By.css('.product'));
+                               let information =await info.findElement(By.css('.product > div:second-child')).getText();
+                               console.log(information);*/
